a7e5ceffc0471477d231ff1a35d987ac1a851f03
harsh_v <96617032+HARSHV10@users.noreply.github.com>
2022-12-05 03:17:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+$(".logo").hover(function(){
+    $(this).css({height:"50px"})
+    setTimeout(() => {
+        $(this).css({height:"70px"})
+    }, 300);
+
+}
+)
+
+// console.log($(".rotate li"))
+
